3c88f3333c25fcd4186adbedb5aa9809517538da
Shekhar Singh <ssingh@supplyvalid.com>
2024-11-14 13:23:17 +0530

    shekhar commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using EmployeeAuthentication.Models;
+using EmployeeAuthentication.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAuthentication.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] UserRegistrationRequest request)
+        {
+            if (_userService.Register(request.Username, request.Password))
+            {
+                return Ok("User registered successfully.");
+            }
+            else
+            {
+                return BadRequest("User already exists.");
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserLoginRequest request)
+        {
+            if (_userService.Login(request.Username, request.Password))
+            {
+                return Ok("Login successful.");
+            }
+            else
+            {
+                return Unauthorized("Invalid username or password.");
+            }
+        }
+    }
+}
+
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeAuthentication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAuthentication.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+}
